dc993d3dd2c180ea586a0683b5a235964be3f520
Riccardo C <rusko@palladius.it>
2011-01-23 14:55:19 +0000

    0.4.1 compiles!

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 # Include hook code here
 
 $RIC_ADDONS_HISTORY = [ # Array of Hash style
-    #VER        #DATE       # Description :)
-    { :ver => '0.3.2',  :date =>'20110122', :desc => 'Moved jeweler in right place. Added skel/ wonderful dir' } ,
-    { :ver => '0.3.1',  :date =>'20110122', :desc => 'Jeweled. Made app/ dir with also metal/. Preparing Thor script to deploy' } ,
-    { :ver => '0.2.2',  :date =>'20110122', :desc => 'Great changes ahead. Still I have the bad symlink though :(' } ,
-    { :ver => '0.2.1a', :date =>'20110121', :desc => 'Corrected typo' } ,
-    { :ver => '0.2.1',  :date =>'20110121', :desc => 'Copied from ric_plugin v0.1b (as it deserved to be copied!)' } ,
+  { :ver => '0.4.1',  :date =>'20110122', :desc => 'Compiles!' } ,
+  { :ver => '0.3.2',  :date =>'20110122', :desc => 'Moved jeweler in right place. Added skel/ wonderful dir' } ,
+  { :ver => '0.3.1',  :date =>'20110122', :desc => 'Jeweled. Made app/ dir with also metal/. Preparing Thor script to deploy' } ,
+  { :ver => '0.2.2',  :date =>'20110122', :desc => 'Great changes ahead. Still I have the bad symlink though :(' } ,
+  { :ver => '0.2.1a', :date =>'20110121', :desc => 'Corrected typo' } ,
+  { :ver => '0.2.1',  :date =>'20110121', :desc => 'Copied from ric_plugin v0.1b (as it deserved to be copied!)' } ,
 ]
 
 $RIC_ADDONS_VER  =  $RIC_ADDONS_HISTORY.first[:ver]  # version
